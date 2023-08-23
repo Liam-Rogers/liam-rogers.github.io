c95e20752745071b107565ee5ce14075d7225ba2
Liam Rogers <65124508+Liam-Rogers@users.noreply.github.com>
2023-08-23 23:22:55 +0100

    z

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-// JavaScript for dynamically creating blog posts
-
-const createBlogPostLink = (postNumber) => {
+const createBlogPostLink = (postNumber, title, imageSrc) => {
     const blogLink = document.createElement("a");
     blogLink.href = `blog/blogpost${postNumber}.html`;
-    blogLink.textContent = `Blog Post ${postNumber}`;
+    blogLink.classList.add('post-preview-link'); // Add a CSS class for styling
+    blogLink.innerHTML = `
+        <div class="post-preview">
+            <img src="${imageSrc}" alt="Blog Post Image" class="thumbnail">
+            <h3>${title}</h3>
+        </div>`;
     return blogLink;
 };
 
-const appendBlogPost = (postNumber) => {
-    const blogList = document.querySelector("#blog-list");
-    const blogPost = document.createElement("li");
-    blogPost.appendChild(createBlogPostLink(postNumber));
-    blogList.appendChild(blogPost);
+
+const fetchTitleAndImageFromBlogPost = (postNumber) => {
+    return fetch(`blog/blogpost${postNumber}.html`)
+        .then(response => response.text())
+        .then(html => {
+            const tempElement = document.createElement('div');
+            tempElement.innerHTML = html;
+            
+            const h1Content = tempElement.querySelector('h1').textContent;
+            const imgElement = tempElement.querySelector('img'); // Get the <img> element
+            const imgSrc = imgElement ? imgElement.getAttribute('src') : null; // Get the image src
+
+            return { title: h1Content, imageSrc: imgSrc };
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
 };
 
-// Add new blog post links here
-appendBlogPost(1);
-appendBlogPost(2);
-// You can continue to append more blog posts as needed
+const appendBlogPostLinksAndImages = async () => {
+    const numberOfPosts = 2; // Adjust this to the number of your blog posts
+    const blogList = document.querySelector("#blog-list");
+
+    for (let postNumber = 1; postNumber <= numberOfPosts; postNumber++) {
+        const { title, imageSrc } = await fetchTitleAndImageFromBlogPost(postNumber);
+        const blogLink = createBlogPostLink(postNumber, title, imageSrc);
+
+        const blogPost = document.createElement("li");
+        blogPost.appendChild(blogLink);
+        blogList.appendChild(blogPost);
+    }
+};
+
+// Call the function to fetch and display the list of blog post links and images
+appendBlogPostLinksAndImages();
