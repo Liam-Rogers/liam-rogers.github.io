bd886d2bd88ed2ded9212ee3ad1ed64b9e389947
Liam Rogers <65124508+Liam-Rogers@users.noreply.github.com>
2023-08-25 19:49:08 +0100

    z

--- script.js
+++
@@ -1,6 +0,0 @@
-const navList = document.querySelector('.nav-list');
-const navToggle = document.querySelector('.logo');
-
-navToggle.addEventListener('click', () => {
-    navList.classList.toggle('show');
-});
